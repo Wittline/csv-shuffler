7f70dfd2f820c6a082af13b761e1d87cf9a80cf0
Wittline <8701464+Wittline@users.noreply.github.com>
2022-07-17 13:55:50 -0500

    Updating csv shuffler

---
+++ csv-lines-shuffler/shufflecsv.py
@@ -0,0 +1,43 @@
+import argparse
+import sys
+import os
+import time
+import numpy as np
+import mmap
+
+
+class ShuffleCSV:
+
+    def __init__(self, input_file, header = True, batch_size = 1000):
+        self.input_file = input_file
+        self.header = header
+        self.batch_size = batch_size
+
+
+    def shuffle_csv(self):        
+
+        with open(self.input_file, mode="r", encoding = "ISO-8859-1") as file_obj:
+            with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as file_map:
+
+                lines = file_map.read().decode("ISO-8859-1").splitlines()
+                
+                if self.header:
+                    header = lines[0]
+                                
+                                
+                lines = lines[1:]
+
+                
+                if self.batch_size <= len(lines):
+                    self.batch_size = len(lines)
+
+                np.random.shuffle(lines)
+
+        with open(self.input_file, 'w') as f:
+            
+            f.write(header + '\n')
+
+            for batch in range(0, len(lines), self.batch_size):                
+                f.write('\n'.join([line for line in lines[batch: batch + self.batch_size]]))
+
+        return len(lines)      
